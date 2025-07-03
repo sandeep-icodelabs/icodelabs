b7ff5710c336db5bbbcd0d77688c47efe816de3f
Sandeep <sandeep@icodelabs.co>
2025-07-03 17:23:25 +0530

    changes

--- src/services/service.js
+++ src/services/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL:"https://api.icodestaging.in/api"
 });
 
 http.interceptors.request.use(
